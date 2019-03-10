68c17580b9d8fa50f1ec3e92fbf59af1759a8478
J9DM <jdawicki@pennmedicine.upenn.edu>
2019-03-10 19:38:25 -0400

    Updated SimpleSpellGame with HashMap to store number of guesses.

--- SimpleSpellGame.java
+++ SimpleSpellGame.java
@@ -14,8 +14,8 @@ class SimpleSpellGame {
             words.add(word);
         }
         
-        // Will eventually want a HashMap to store number of guesses for each word.
-        int numGuesses = 0;
+        // Create HashMap to store number of guesses for each word.
+        HashMap<String, Integer> numGuesses = new HashMap<String, Integer>();
         
         // GameHelper to help with getting user input.
         GameHelper helper = new GameHelper();
@@ -29,12 +29,13 @@ class SimpleSpellGame {
             SimpleWord wordToGuess = words.get(randomNum);
             
             // Prompt user for a guess. Check guess against correct spelling.
-            String userInput = helper.getUserInput(("Spell the word " + wordToGuess.getSpelling()));
+            String spelled = wordToGuess.getSpelling();
+            String userInput = helper.getUserInput(("Spell the word " + spelled));
             String result = wordToGuess.checkSpelling(userInput);
             
             // Remove correctly spelled word from ArrayList and store numOfGuesses.
             if (result.equals("correct")) {
-                numGuesses = wordToGuess.getNumGuesses();
+                numGuesses.put(spelled, wordToGuess.getNumGuesses());
                 words.remove(wordToGuess);
                 System.out.println("You are correct.");
             } else {
